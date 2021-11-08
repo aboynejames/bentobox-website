ac1f2af8e167ad7ecb92a17f4b9b5bfc600850c1
aboynejames <james@aboynejames.co.uk>
2021-11-08 21:14:04 +0000

    bentobox website

---
+++ src/components/BboxLearn.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container grid-list-lg>
+    <v-layout row>
+        <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5 color blue lighten-5">Open Source Toolkit for Collective Intelligence</v-flex>
+    </v-layout>
+    <v-layout row align="center" justify="center">
+        <v-flex xs12>
+          <v-img src="../assets/bbdds.png" max-width="400" >
+          </v-img>
+        </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0 font-weight-bold">Now</h3>
+                <div style="word-break: break-word" class="spacing-playground pa-6 ">
+                  Use historical data to make observations.
+                </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0 font-weight-bold">Future</h3>
+                <div style="word-break: break-word" class="spacing-playground pa-3">
+                  Look forward to future time periods.
+                </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0 font-weight-bold">Now network</h3>
+                <div style="word-break: break-word" class="spacing-playground pa-3">
+                  Combine networks of historical data.
+                </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm12 md12>
+        <v-card>
+          <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0 font-weight-bold">Future Network</h3>
+                <div class="spacing-playground pa-3">
+                  Aggregate data to look forward in time.
+                </div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TaapgLearn'
+}
+</script>
+
+<style scoped>
+</style>
